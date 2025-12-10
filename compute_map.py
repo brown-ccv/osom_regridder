5af965b00cb87d79add8d3ea0ad527fa2698fb01
Anna Murphy <anna_murphy1@brown.edu>
2025-12-10 17:16:26 -0500

    more functions, unannotated

--- compute_map.py
+++ compute_map.py
@@ -8,6 +8,7 @@ Functions:
   determine_bounds: Determines grid bounds from the given OSOM dataset.
 """
 
+import math
 import numpy as np
 from typing import Tuple
 
@@ -32,10 +33,28 @@ def determine_bounds(coordinate_grid: np.ndarray, cell_count: int) -> Tuple[floa
   cell_size = coordinate_range / cell_count
   return (coordinate_min, coordinate_max, coordinate_range, cell_size)
 
-def foo():
+def compute_coordinate_indecies(coordinate_min, coordinate_max, coordinate_cell_size):
+  return np.arange(coordinate_min, coordinate_max, coordinate_cell_size)
+
+def compute_coordinate_distance(lat_a, lon_a, lat_b, lon_b):
+  return math.sqrt((abs(lat_a - lat_b) ** 2) + (abs(lon_a - lon_b) ** 2))
+
+def compute_distance_from_coordinate_to_osom_grid(point_lat, point_lon, grid_lat, grid_lon):
+  """
+  create 2d array the size of etiher grid_lat or grid_lon.
+  then, naive loop to determine the distance using compute_coordinate_distance.
+  take the value and put it in the output array.
+  return the output array.
+  """
   pass
 
 if __name__ == "__main__":
+  lat_len = 16
+  lon_len = 26
   lat, lon, mask, bathymetry = import_grid("data/osom_grid.nc")
-  lat_min, lat_max, lat_range, lat_cell_size = determine_bounds(lat, 16)
-  print(lat_min, lat_max, lat_range, lat_cell_size)
+  lat_min, lat_max, lat_range, lat_cell_size = determine_bounds(lat, lat_len)
+  lon_min, lon_max, lon_range, lon_cell_size = determine_bounds(lon, lon_len)
+  lat_indecies = compute_coordinate_indecies(lat_min, lat_max, lat_cell_size)
+  lon_indecies = compute_coordinate_indecies(lon_min, lon_max, lon_cell_size)
+  print(lat_indecies, lon_indecies)
+
