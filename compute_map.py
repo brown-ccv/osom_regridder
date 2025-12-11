2c2ebfc672cb0d96ef5a8ebd4fe0d47b2c8fa84e
Anna Murphy <anna_murphy1@brown.edu>
2025-12-11 11:13:27 -0500

    format

--- compute_map.py
+++ compute_map.py
@@ -1,5 +1,5 @@
 """
-This module contains utility functions to create map files between raw OSOM 
+This module contains utility functions to create map files between raw OSOM
 dataset and regridded latitude / longitude datasets. Given the OSOM grid file
 and output x / y dimensions, this will generate a map to determine RAW OSOM
 points map to which output latitude / longitude cells.
@@ -14,47 +14,55 @@ from typing import Tuple
 
 from file_input import import_grid
 
-def determine_bounds(coordinate_grid: np.ndarray, cell_count: int) -> Tuple[float, float, float, float]:
-  """
-  Utility function to determine the bounds of the OSOM grid (either latitude or longitude).
 
-  Parameters:
-    coordinate_grid (np.ndarray): The coordinate grid provided by the OSOM 
-      grid file. Either latitude or longitude grid.
-    cell_count (int): The number of output cells in this dimension.
-  
-  Returns:
-    Tuple[float, float, float, float]: A tuple containing the minimum, maximum,
-      and range of the dataset, as well as the cell size on the provided axis.
-  """
-  coordinate_min = np.min(coordinate_grid)
-  coordinate_max = np.max(coordinate_grid)
-  coordinate_range = coordinate_max - coordinate_min
-  cell_size = coordinate_range / cell_count
-  return (coordinate_min, coordinate_max, coordinate_range, cell_size)
+def determine_bounds(
+    coordinate_grid: np.ndarray, cell_count: int
+) -> Tuple[float, float, float, float]:
+    """
+    Utility function to determine the bounds of the OSOM grid (either latitude or longitude).
+
+    Parameters:
+      coordinate_grid (np.ndarray): The coordinate grid provided by the OSOM
+        grid file. Either latitude or longitude grid.
+      cell_count (int): The number of output cells in this dimension.
+
+    Returns:
+      Tuple[float, float, float, float]: A tuple containing the minimum, maximum,
+        and range of the dataset, as well as the cell size on the provided axis.
+    """
+    coordinate_min = np.min(coordinate_grid)
+    coordinate_max = np.max(coordinate_grid)
+    coordinate_range = coordinate_max - coordinate_min
+    cell_size = coordinate_range / cell_count
+    return (coordinate_min, coordinate_max, coordinate_range, cell_size)
+
 
 def compute_coordinate_indecies(coordinate_min, coordinate_max, coordinate_cell_size):
-  return np.arange(coordinate_min, coordinate_max, coordinate_cell_size)
+    return np.arange(coordinate_min, coordinate_max, coordinate_cell_size)
+
 
 def compute_coordinate_distance(lat_a, lon_a, lat_b, lon_b):
-  return math.sqrt((abs(lat_a - lat_b) ** 2) + (abs(lon_a - lon_b) ** 2))
+    return math.sqrt((abs(lat_a - lat_b) ** 2) + (abs(lon_a - lon_b) ** 2))
 
-def compute_distance_from_coordinate_to_osom_grid(point_lat, point_lon, grid_lat, grid_lon):
-  """
-  create 2d array the size of etiher grid_lat or grid_lon.
-  then, naive loop to determine the distance using compute_coordinate_distance.
-  take the value and put it in the output array.
-  return the output array.
-  """
-  pass
+
+def compute_distance_from_coordinate_to_osom_grid(
+    point_lat, point_lon, grid_lat, grid_lon
+):
+    """
+    create 2d array the size of etiher grid_lat or grid_lon.
+    then, naive loop to determine the distance using compute_coordinate_distance.
+    take the value and put it in the output array.
+    return the output array.
+    """
+    pass
+
 
 if __name__ == "__main__":
-  lat_len = 16
-  lon_len = 26
-  lat, lon, mask, bathymetry = import_grid("data/osom_grid.nc")
-  lat_min, lat_max, lat_range, lat_cell_size = determine_bounds(lat, lat_len)
-  lon_min, lon_max, lon_range, lon_cell_size = determine_bounds(lon, lon_len)
-  lat_indecies = compute_coordinate_indecies(lat_min, lat_max, lat_cell_size)
-  lon_indecies = compute_coordinate_indecies(lon_min, lon_max, lon_cell_size)
-  print(lat_indecies, lon_indecies)
-
+    lat_len = 16
+    lon_len = 26
+    lat, lon, mask, bathymetry = import_grid("data/osom_grid.nc")
+    lat_min, lat_max, lat_range, lat_cell_size = determine_bounds(lat, lat_len)
+    lon_min, lon_max, lon_range, lon_cell_size = determine_bounds(lon, lon_len)
+    lat_indecies = compute_coordinate_indecies(lat_min, lat_max, lat_cell_size)
+    lon_indecies = compute_coordinate_indecies(lon_min, lon_max, lon_cell_size)
+    print(lat_indecies, lon_indecies)
--- output.py
+++ output.py
@@ -3,78 +3,99 @@ import netCDF4 as nc
 from PIL import Image
 from typing import Tuple
 
-def normalize_linear(value: float, input_scale_min: float, input_scale_max: float, output_scale_min: float, output_scale_max: float) -> float:
-  """
-  Utility function to linearly normalize model data for the creation of output images.
 
-  Parameters:
-    value (float): The value to be normalized.
-    input_scale_min (float): The minimum value of the input range.
-    input_scale_max (float): The maximum value of the input range.
-    output_scale_min (float): The minimum value of the output range.
-    output_scale_max (float): The maximum value of the output range.
-
-  Returns:
-      float: The normalized value scaled to the output range.
+def normalize_linear(
+    value: float,
+    input_scale_min: float,
+    input_scale_max: float,
+    output_scale_min: float,
+    output_scale_max: float,
+) -> float:
     """
-  standard_normalization = (value - input_scale_min) / (input_scale_max - input_scale_min)
-  return ((output_scale_max - output_scale_min) * standard_normalization) + output_scale_min
+    Utility function to linearly normalize model data for the creation of output images.
+
+    Parameters:
+      value (float): The value to be normalized.
+      input_scale_min (float): The minimum value of the input range.
+      input_scale_max (float): The maximum value of the input range.
+      output_scale_min (float): The minimum value of the output range.
+      output_scale_max (float): The maximum value of the output range.
+
+    Returns:
+        float: The normalized value scaled to the output range.
+    """
+    standard_normalization = (value - input_scale_min) / (
+        input_scale_max - input_scale_min
+    )
+    return (
+        (output_scale_max - output_scale_min) * standard_normalization
+    ) + output_scale_min
+
 
 def compute_normalization_scale(dataset: np.ndarray) -> Tuple[float, float]:
-  """
-  Determines the minimum and maximum values of the processed dataset.
+    """
+    Determines the minimum and maximum values of the processed dataset.
 
-  Parameters:
-    dataset (np.ndarray): A regridded OSOM dataset presented as a 2D NumPy array.
+    Parameters:
+      dataset (np.ndarray): A regridded OSOM dataset presented as a 2D NumPy array.
 
-  Returns:
-    Tuple[float, float]: A tuple containing the minimum and maximum values of the dataset.
-  """
-  values = np.unique(dataset[~np.isnan(dataset)])
-  return (np.min(values), np.max(values))
+    Returns:
+      Tuple[float, float]: A tuple containing the minimum and maximum values of the dataset.
+    """
+    values = np.unique(dataset[~np.isnan(dataset)])
+    return (np.min(values), np.max(values))
+
 
 def create_image(dataset, output_dim_x, output_dim_y):
-  '''
-  Transforms the dataset into a bitmap image. All NaN entries in the dataset are written as
-  transparent pixels, and everything is normalized on a black -> purple color scale. 
-  '''
-  image = Image.new(mode="RGBA", size=(output_dim_x, output_dim_y), color = (0, 0, 0, 0))
-  output_min, output_max = compute_normalization_scale(dataset)
-  for x in range(output_dim_x):
-    for y in range(output_dim_y):
-      value = dataset[x][y]
-      if not np.isnan(value):
-        color_as_value = int(normalize_linear(value, output_min, output_max, 0, 255))
-        image.putpixel((x, output_dim_y - 1 - y), (color_as_value, 0, color_as_value, 255))
-  return image
+    """
+    Transforms the dataset into a bitmap image. All NaN entries in the dataset are written as
+    transparent pixels, and everything is normalized on a black -> purple color scale.
+    """
+    image = Image.new(
+        mode="RGBA", size=(output_dim_x, output_dim_y), color=(0, 0, 0, 0)
+    )
+    output_min, output_max = compute_normalization_scale(dataset)
+    for x in range(output_dim_x):
+        for y in range(output_dim_y):
+            value = dataset[x][y]
+            if not np.isnan(value):
+                color_as_value = int(
+                    normalize_linear(value, output_min, output_max, 0, 255)
+                )
+                image.putpixel(
+                    (x, output_dim_y - 1 - y), (color_as_value, 0, color_as_value, 255)
+                )
+    return image
+
 
 def save_image(image: Image, image_path: str) -> None:
-  """
-  Utility function to write a create image to disk.
+    """
+    Utility function to write a create image to disk.
 
-  Parameters:
-    image (Image): Pillow Image object.
-    image_path (string): Path where image will be saved.
-  
-  Returns:
-      None: Image is written to disk.
-  """
-  image.save(image_path)
+    Parameters:
+      image (Image): Pillow Image object.
+      image_path (string): Path where image will be saved.
+
+    Returns:
+        None: Image is written to disk.
+    """
+    image.save(image_path)
+
 
 def save_dataset(dataset: np.ndarray, variable: str, file_path: str) -> None:
-  """
-  Utility function to write the dataset to disk as a NetCDF file. 
+    """
+    Utility function to write the dataset to disk as a NetCDF file.
 
-  Parameters:
-    dataset (np.ndarray): Regridded OSOM dataset.
-    variable (str): Variable being saved in this NetCDF file.
-    file_path (str): Path where the dataset will be written.
+    Parameters:
+      dataset (np.ndarray): Regridded OSOM dataset.
+      variable (str): Variable being saved in this NetCDF file.
+      file_path (str): Path where the dataset will be written.
 
-  Returns:
-    None: Dataset is written to disk.
-  """
-  with nc.Dataset(file_path, 'w', 'NETCDF4') as output_file:
-    output_file.createDimension('rows', dataset.shape[0])
-    output_file.createDimension('cols', dataset.shape[1])
-    data_var = output_file.createVariable(variable, 'float32', ('rows', 'cols'))
-    data_var[:] = dataset
+    Returns:
+      None: Dataset is written to disk.
+    """
+    with nc.Dataset(file_path, "w", "NETCDF4") as output_file:
+        output_file.createDimension("rows", dataset.shape[0])
+        output_file.createDimension("cols", dataset.shape[1])
+        data_var = output_file.createVariable(variable, "float32", ("rows", "cols"))
+        data_var[:] = dataset
