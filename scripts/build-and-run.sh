dead1977c87c2f6185735d67f2406faf616e492f
Anna Murphy <anna_murphy1@brown.edu>
2025-12-15 16:31:34 -0500

    remove old build versions and install the newest built version when running the script

--- scripts/build-and-run.sh
+++ scripts/build-and-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+rm dist/osom_regridder-*
 uv build
-uv pip install dist/osom_regridder-0.1.0-py3-none-any.whl
+uv pip install dist/osom_regridder-*.whl
 python3 -m osom_regridder regrid data/osom_grid.nc data/osom_data_6210.nc --variable temp
 python3 -m osom_regridder display out/osom_data_6210_temp@1.nc temp
