e5a814a0fe0ade6dca74ce9fcac2d7dda3f96a5a
Anna Murphy <anna_murphy1@brown.edu>
2026-03-05 16:00:24 -0500

    remove comments

--- scripts/georeference.sh
+++ scripts/georeference.sh
@@ -4,6 +4,3 @@ gdal_translate out/osom_data_6210_temp@1.tif out/6210_temp.tif -of GTiff -a_srs 
 gdal_translate out/6210_temp.tif out/6210_temp.mbtiles -of MBTILES
 gdaladdo -r average out/6210_temp.mbtiles 2 4 8 16 32 64 128 256
 
-
-#gdal_translate out/osom_data_6210_temp@1.tif out/6210_temp.mbtiles -of MBTILES -a_srs EPSG:4326 -a_ullr -72.7 41.9 -69.96 40.5
-#gdaladdo -r average out/6210_temp.mbtiles 2 4 8 16 32 64 128
