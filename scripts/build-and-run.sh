0914f9f651aba381f88f7591bb706e5114897afc
Anna Murphy <anna_murphy1@brown.edu>
2026-03-06 11:26:26 -0500

    comment out things

--- scripts/build-and-run.sh
+++ scripts/build-and-run.sh
@@ -15,7 +15,7 @@ uv pip install dist/osom_regridder-*.whl
 
 #python3 -m osom_regridder regrid-at-timepoint --help
 python3 -m osom_regridder regrid-at-timepoint data/osom_grid.nc data/osom_data_6210.nc --variable temp
-python3 -m osom_regridder display out/osom_data_6210_temp@1.nc temp
+#python3 -m osom_regridder display out/osom_data_6210_temp@1.nc temp
 
 # Regrid Dataset at all timepoints and create images
 
